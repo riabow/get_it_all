cf7f71f81bbc03b3e61afc7ed3f35b7cad1cbefd
riabow <riabow@mail.ru>
2016-07-23 02:25:17 +0300

    1

--- a3.py
+++ a3.py
@@ -1,14 +1,15 @@
 import requests
+import os
 
 def GetSubString(body, str1, str2):
     if str1 in body:
         bg = body.find(str1)
         if str2 in body:
-            en = body.find(str2)
+            en = body.find(str2, bg )
             if en>0:
                 #print bg
                 #print en
-                return ( body[bg:en])
+                return ( body[bg+len(str1):en])
             else:
                 return ('en<0')
         else:
@@ -23,13 +24,17 @@ class a53s():
 
     session =""
     passdata = {'login': 'NosovE',
-            'pass': '12345'}
+                 'pass': '12345',}
+
+
+
     def f(self):
         return 'hello world'
     def login(self):
         URL_login = "http://a53s.ru/login.php"
         self.session = requests.session()
         r = self.session.post(URL_login, data=self.passdata)
+        #print self.session.
 
     def GetReport(self):
         URL = "http://a53s.ru/sort/poraskl3.php"
@@ -37,14 +42,19 @@ class a53s():
         #print "code:"+str( r.status_code)
 
         #print "headers:"+str(r.headers)
+        TMPfilepath = 'file_name.html'
+        f = open(TMPfilepath, 'wb')
+        f.write(r.content)
+        f.close()
+        os.startfile(TMPfilepath)
+
         return( r.content)
 
 ll = a53s()
 ll.login()
 body =ll.GetReport()
 MyD = GetSubString(body, 'Array','<table cellpadding="0" cellspacing="0" border="0" id="table" class="sortable" style="padding: 2px;">')
-sql = GetSubString(body, '''<BR><BR><BR><br />''','''<BR><BR><BR>				<tr>
-					<td class="brd">''')
+sql = GetSubString(body, '''<BR><BR><BR><br />''','''<BR><BR><BR>				<tr>''')
 
 
 
